ffa0e7ac147b2f05e5b9c27cf3cd6570c73ba8ff
a044887852v <a044887852v@s09-pc08>
2016-01-21 16:34:50 +0100

    Version 1

---
+++ src/estadistica/modelo/Articulo.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estadistica.modelo;
+
+/**
+ *
+ * @author a048598270y
+ */
+public class Articulo {
+
+    private final String nombre;
+    private final int cantidad;
+    private final float precio_unidad;
+
+    public Articulo(String nombre, int cantidad, float precio_unidad) {
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.precio_unidad = precio_unidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public float getPrecio_unidad() {
+        return precio_unidad;
+    }
+
+    public float getPrecioTotal() {
+        return cantidad * precio_unidad;
+
+    }
+
+    public float getPrecioTotalIVA() {
+        return (cantidad * precio_unidad) * 1.21f;
+    }
+
+}
